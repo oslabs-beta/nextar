cf213ddc7fa5b00579a238ab30557a3da75a5e42
Julia Shi <juliajshi@gmail.com>
2023-09-13 08:53:38 -0400

    added d3 linecharts

--- components/CreateGraph.jsx
+++ components/CreateGraph.jsx
@@ -4,43 +4,14 @@ import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 
 export default function CreateGraph(props) {
-
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   setData(JSON.parse(localStorage.getItem('FCParray')));
-  //   // Perform localStorage action
-  //   const item = localStorage.getItem('key')
-  // }, [])
-
-  // const [data] = useState([25, 50, 35, 15, 99, 10]);
-  // const [data] = useState([287.9, 244.6, 274.5, 282.6, 176.3, 174.3, 990.5, 990.5, 749.9, 749.9, 772.5, 772.5, 226.3, 916.4])
   const data = props.array;
+  console.log(props.array)
   const svgRef = useRef();
-
-
-  // const createGraph = async () => {
-  //   // data = JSON.parse(localStorage.getItem('FCParray')) || [];
-
-  //   // set the dimensions and margins of the graph
-  //   var margin = { top: 20, right: 20, bottom: 50, left: 70 },
-  //       width = 960 - margin.left - margin.right,
-  //       height = 500 - margin.top - margin.bottom;
-  //   // append the svg object to the body of the page
-  //   var svg = d3.select("body").append("svg")
-  //       .attr("width", width + margin.left + margin.right)
-  //       .attr("height", height + margin.top + margin.bottom)
-  //       .append("g")
-  //       .attr("transform", `translate(${margin.left},     ${margin.top})`);
-
-
-  // }
-
 
   useEffect(() => {
     // set up svg
-    const w = 1000;
-    const h = 100;
+    const w = 500;
+    const h = 300;
 
     // const margin = { top: 40, right: 80, bottom: 60, left: 50 },
     // width = 960 - margin.left - margin.right,
@@ -49,28 +20,34 @@ export default function CreateGraph(props) {
     const svg = d3.select(svgRef.current)
       .attr('width', w)
       .attr('height', h)
-      .style('background', '#d3d3d3')
+      .style('background', '#e1ebf7')
       .style('margin-top', '50')
       .style('overflow', 'visible');
 
 
     // set up scaling
+    
+    const max = d3.max(data);
+    // const min = data.sort()[0]
     const xScale = d3.scaleLinear()
-      .domain([0, data.length - 1])
+      .domain([0,data.length - 1])
       .range([0,w]);
     const yScale = d3.scaleLinear()
-      .domain([0, h])
+      .domain([0, max])
       .range([h,0]);
     const generateScaledLine = d3.line()
       .x((d, i) => xScale(i))
       .y(yScale)
-      .curve(d3.curveCardinal);
+      .curve(d3.curveLinear);
 
+      // deleting old svg
+      svg.selectAll("svg > *").remove();
+      
     // setting up the axis
     const xAxis = d3.axisBottom(xScale)
       .ticks(data.length)
       .tickFormat(i => i + 1);
-    const yAxis = d3.axisLeft(xScale)
+    const yAxis = d3.axisLeft(yScale)
       .ticks(10);
     svg.append('g')
       .call(xAxis)
@@ -80,12 +57,13 @@ export default function CreateGraph(props) {
 
 
     // setting up the data
-    svg.selectAll('.line')
+    svg.append('path')
       .data([data])
       .join('path')
       .attr('d', d => generateScaledLine(d))
       .attr('fill', 'none')
       .attr('stroke', 'blue')
+      .style("stroke-width", 4)
 
   }, [data]);
 
