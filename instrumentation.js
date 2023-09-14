495cdde78c33f86c3b9f44ed14b570ce26eb26e0
amoomoo <uppalamulya@gmail.com>
2023-09-14 14:26:43 -0400

    nextar/npmpackge

--- instrumentation.js
+++ instrumentation.js
@@ -1,5 +1,7 @@
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
       await import('./instrumentation.node.js')
+      const {startTrace} = await import('@nextar/tracer');
+      startTrace();
     }
   }
---
+++ nextar-server.js
@@ -0,0 +1,3 @@
+import startServer from '@nextar/server';
+
+startServer();
